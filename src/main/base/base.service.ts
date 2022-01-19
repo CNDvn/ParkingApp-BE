4ab5960b4fa69045ca950c80991ec1968f2868db
DQLok <wkhhkw01@gmail.com>
2022-01-19 22:02:02 +0700

    change base service

--- src/main/base/base.service.ts
+++ src/main/base/base.service.ts
@@ -1,16 +1,9 @@
-import { LoggerService } from '@nestjs/common';
 import { DeepPartial, DeleteResult, Repository } from 'typeorm';
 import { EntityId } from 'typeorm/repository/EntityId';
 import BaseEntity from './base.entity';
 
-export class BaseService<T extends BaseEntity, R extends Repository<T>> {
-  protected readonly repository: R;
-  protected readonly logger: LoggerService;
-
-  constructor(repository: R, logger: LoggerService) {
-    this.repository = repository;
-    this.logger = logger;
-  }
+export class BaseService<T extends BaseEntity> {
+  constructor(private readonly repository: Repository<T>) {}
 
   async getAll(): Promise<T[]> {
     return await this.repository.find();
@@ -28,7 +21,7 @@ export class BaseService<T extends BaseEntity, R extends Repository<T>> {
     return await this.repository.save(data);
   }
 
-  async updata(id: EntityId, data: DeepPartial<T>): Promise<T> {
+  async update(id: EntityId, data: DeepPartial<T>): Promise<T> {
     await this.repository.update(id, data);
     return await this.findById(id);
   }
