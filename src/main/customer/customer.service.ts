a9937558dac0e5188b1da1f5e3acced9f00e61cd
khoa <npdkhoa2311@gmail.com>
2022-02-07 21:05:53 +0700

    PAB-10

--- src/main/customer/customer.service.ts
+++ src/main/customer/customer.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { BaseService } from '../base/base.service';
 import Customer from './customer.entity';
 import { CustomerRepository } from './customer.repository';
