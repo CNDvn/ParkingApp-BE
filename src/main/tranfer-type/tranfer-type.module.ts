d224c61185f96e23253776f3975c5baee2f49d08
Code Never Die <banhduchieucode@gmail.com>
2022-03-10 15:33:05 +0700

    delete cash transfer and transfer type

--- src/main/tranfer-type/tranfer-type.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TranferTypeService } from './tranfer-type.service';
-import { TranferTypeController } from './tranfer-type.controller';
-
-@Module({
-  controllers: [TranferTypeController],
-  providers: [TranferTypeService],
-})
-export class TranferTypeModule {}
